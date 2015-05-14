0f31779fbbb8e6e265dff81bb5a65e4b00d70832
Guilherme  Ventura <guilherme@fanatee.com>
2015-05-14 18:44:35 -0300

    Pagina como registro box + circulos

--- como_registro.php
+++ como_registro.php
@@ -15,13 +15,32 @@
                 <?php include("header.html"); ?>
 			</header>
 			<section class="section-padrao sect-como">
-                <div class="align-h1-como">
-                    <div class="circulo circ-red1"><span class="icon icon-white">?</span></div>
-                    <h1 class="desq-red">COMO REGISTRO?</h1>
-                    <h2 class="desq-red">Confira os 4 passos para o registro da sua marca:</h2>
+                <div class="box-group">
+                    <div class="align-h1-como">
+                        <div class="circulo circ-red1"><span class="icon icon-white">?</span></div>
+                        <h1 class="desq-red">COMO REGISTRO?</h1>
+                        <h2 class="desq-red">Confira os 4 passos para o registro da sua marca:</h2>
+                    </div><br><br>
+
+                    <div class="box b-azul left">
+                        <h2>PESQUISE SUA MARCA CONOSCO</h2>
+                        <p>No prazo máximo de 24 horas você receberá em seu e-mail um Login e Senha para visualizar o Resultado de Disponibilidade de Registro de sua Marca que será devidamente analisado e esclarecido por um de nossos consultores;</p>
+                    </div>
+                    <div class="circulo b-azul circ-left"><span class="icon icon-white">1º</span></div>
+                    <div class="box b-red left dir-box">
+                    </div>
+                    <div class="circulo b-red circ-left"><span class="icon icon-white">2º</span></div>
+                    <div class="box-space clearfix"></div>
+                    <div class="box b-red left">
+                    </div>
+                    <div class="circulo b-red circ-left"><span class="icon icon-white">3º</span></div>
+                    <div class="box b-azul left dir-box">
+                    </div>
+                    <div class="circulo b-azul circ-left"><span class="icon icon-white">4º</span></div>
                 </div>
+                <div class="box-space clearfix"></div>
+                <?php include("pesquise_marca.html"); ?>
             </section>
-            <?php include("pesquise_marca.html"); ?>
 			<?php include("footer.html"); ?>
 		</div>
 	</body>
