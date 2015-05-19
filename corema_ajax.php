f575c6f82c8a5c14915d3f3490f76b2798902db6
Guilherme  Ventura <guilherme@fanatee.com>
2015-05-19 16:54:40 -0300

    Headers mail() php

--- corema_ajax.php
+++ corema_ajax.php
@@ -3,9 +3,14 @@ if($_POST){
     $name = $_POST['name'];
     $email = $_POST['email'];
     $message = $_POST['message'];
+    $to      = 'venturalp@gmail.com';
+    $subject = 'Contato - Corema';
+    $headers = 'From: '. $email . "\r\n" .
+        'Reply-To: '. $email . "\r\n";
+
 
 //send email
-    if (mail("venturalp@gmail.com", "Testando envio de email" .$email, $message))
+    if (mail($to, $subject, $message . "\nUsuário: " . $name , &headers))
         echo "Email enviado com sucesso!";
     else
         echo "Erro ao enviar email, tente novamente.";
